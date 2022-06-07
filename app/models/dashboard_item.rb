8fe942ee931980685219d64610c7f063130db33e
Jessie Woods <jessieallyn@gmail.com>
2022-06-07 15:34:08 -0500

    added validations

--- app/models/dashboard_item.rb
+++ app/models/dashboard_item.rb
@@ -1,2 +1,6 @@
 class DashboardItem < ApplicationRecord
+    #add validations here
+    validates :name, presence: true, uniqueness: true;
+    validates :size, presence: true;
+    validates :display_type, presence: true;
 end

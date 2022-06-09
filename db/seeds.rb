461a2df69903f8842cc2c8ffe559437f8c339d92
Jessie Woods <jessieallyn@gmail.com>
2022-06-09 11:29:02 -0500

    added seed data for Salesman_info

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Salesman_info.destroy_all
+
+Salesman_info.create!([
+    {
+        name: "Mako Mori",
+        qty_wire: 9000,
+        part_number: "14NO35",
+        dollar_amount_sold: 0.13031,
+        region: "South"
+    },
+    {
+        name: "Raleigh Becket",
+        qty_wire: 4000,
+        part_number: "14NO35",
+        dollar_amount_sold: 0.13031,
+        region: "South"
+    },
+    {
+        name: "Stacker Pentecost",
+        qty_wire: 15000,
+        part_number: "14NO35",
+        dollar_amount_sold: 0.13031,
+        region: "South"
+    },
+    {
+        name: "Sasha Kaidanovsky",
+        qty_wire: 5000,
+        part_number: "14NO35",
+        dollar_amount_sold: 0.13031,
+        region: "South"
+    }
+])
+
+p "created #{Salesman_info.count} Salesman_info"

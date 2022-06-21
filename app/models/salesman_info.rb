b005bd716406e06376d391997d266d32f10afe48
Nolan Hovis <nolan.hovispersonal@gmail.com>
2022-06-21 13:09:37 -0500

    Seed File

--- app/models/salesman_info.rb
+++ app/models/salesman_info.rb
@@ -1,7 +1,4 @@
 class SalesmanInfo < ApplicationRecord
   validates :name, presence: true
   validates :region, presence: true
-  validates :qty_wire 
-  validates :dollar_amount_sold
-  validates :part_number
 end

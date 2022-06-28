5771db788a278cc1f14741efcd029fdfc4194558
RSandage <90716032+RSandage@users.noreply.github.com>
2022-06-28 13:37:34 -0500

    Added name to custom_dashboards

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_24_194738) do
+ActiveRecord::Schema.define(version: 2022_06_28_183516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2022_06_24_194738) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.index ["user_id"], name: "index_custom_dashboards_on_user_id"
   end
 
